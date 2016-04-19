171d26e5dd4b274ff194b77f0434f8504ac70286
Peter Jang <peter.x.jang@gmail.com>
2016-04-18 20:36:54 -0500

    solutions for all 5 steps of exercise

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -1,7 +1,40 @@
 /* global angular */
 (function() {
   angular.module("app").controller("peopleCtrl", function($scope) {
-    $scope.message = "hello";
+    $scope.people = [
+      {
+        name: "Peter Jang",
+        bio: "fdsafd safdjsa; fdsajfdslafdsla;f sdafdsaf",
+        bioVisible: true
+      },
+      {
+        name: "Jamie Gates",
+        bio: "wepurwoiqpurew ureowpqureiowur weqruowqp rpwe",
+        bioVisible: true
+      },
+      {
+        name: "Jen Wu",
+        bio: "vcnxzv.,mznn vzmv cmnzx.v cxnzm.vnczmv,.z cnzxc.,v ",
+        bioVisible: true
+      }
+    ];
+
+    $scope.toggleBio = function(inputPerson) {
+      inputPerson.bioVisible = !inputPerson.bioVisible;
+    };
+
+    $scope.addPerson = function(inputPersonName, inputPersonBio) {
+      $scope.people.push({
+        name: inputPersonName,
+        bio: inputPersonBio,
+        bioVisible: true
+      });
+    };
+
+    $scope.deletePerson = function(inputIndex) {
+      $scope.people.splice(inputIndex, 1);
+    };
+
     window.$scope = $scope;
   });
 })();
